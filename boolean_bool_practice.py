ce6f039829f01b062464b9ba0a2ce8cb61e51d01
Snow Hsu <shihnunghsu@gmail.com>
2024-05-23 21:15:44 +0800

    boolean bool practice

---
+++ boolean_bool_practice.py
@@ -0,0 +1,11 @@
+# 布林值練習Boolean bool (True/False) 
+# 情境為對照Keelung and Taipei 的月下雨頻率
+# 收穫1：布林值可以應用在比較的情境
+monthly_average_rainning_frequency_at_Keelung = 0.25
+monthly_average_rainning_frequency_at_Taipei = 0.15
+print(monthly_average_rainning_frequency_at_Taipei == 0.25)
+print(monthly_average_rainning_frequency_at_Taipei != 0.25)
+print(monthly_average_rainning_frequency_at_Taipei > 0.25)
+print(monthly_average_rainning_frequency_at_Taipei < 0.25)
+print(monthly_average_rainning_frequency_at_Taipei >= 0.25)
+print(monthly_average_rainning_frequency_at_Taipei <= 0.25)
