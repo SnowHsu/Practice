4d1c278372a80c6713124fea0cfc34b7c27d6704
Snow Hsu <shihnunghsu@gmail.com>
2024-05-23 21:18:59 +0800

    修正:讓程式更好閱讀,增加行數/語法空格

--- Greetingpage.py
+++ Greetingpage.py
@@ -1,7 +1,17 @@
-print('Hello, nice to meet you. Welcome to thh coding lab!')
+# 練習基本語法,以下情境是網站登入頁面
+# 收穫1：coding style-正確空格（＝前後／，後）
+# 收穫2：input用法：與使用者互動，使用者回應前不會結束程式
+# 收穫3：未定義的字串一定要用''在兩側
+# 收穫4：定義的字串若超過1個單詞及需加_　，　例如　my_name = 'SnowHsu'
+# 收穫5：可以使用# 來做註解,#不會顯示在程式裡
+print('Hello, nice to meet you. Welcome to the coding lab!')
 name = input('請輸入您的名字：  ')
-print('Hi',name,'Today is your first day in the lab so we would like to know more about you')
+print('Hi', name, 'Today is your first day in the lab so we would like to know more about you')
+print('       ')
+print('       ')
 age = input('How old are you?　　')
+print('       ')
+print('       ')
 motivation = input('Why do you want to learn python?　　')
 print('       ')
 print('       ')
@@ -9,6 +19,6 @@ print('       ')
 print('       ')
 print('       ')
 print('Great! Now we know you well')
-print('we know you are',age)
-print(motivation)
-print('wish you a interesting learning journey!')
+print('we know you are', age)
+print('And your motivation is', motivation)
+print('Wish you a interesting learning journey!')
